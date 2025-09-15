55ee6d057577fbdd9c0991ce5a0975850a97c7be
Rajmani <rajmani@192.168.1.39>
2025-09-16 03:12:45 +0530

    pom.xml setup completed with opensearch dependencies

---
+++ src/main/java/com/project/documentsearch/DocumentsearchApplication.java
@@ -0,0 +1,13 @@
+package com.project.documentsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/documentsearch/DocumentsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.documentsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
